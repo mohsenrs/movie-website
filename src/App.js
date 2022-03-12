bc526048d541bda292245269a965b7f161758bc2
mohsenrs <mohsen8022@gmail.com>
2022-03-12 10:20:45 +0330

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import './App.scss'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Home from './components/home/Home'
+import MovieDetail from './components/movieDetail/MovieDetail'
+import PageNotFound from './components/pageNotFound/PageNotFound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Switch>
+        <Route path='/'>
+          <Home />
+        </Route>
+        <Route path='movie/:id'>
+          <MovieDetail />
+        </Route>
+        <Route>
+          <PageNotFound />
+        </Route>
+      </Switch>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/common/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIkey = 'cd8f1b8'
